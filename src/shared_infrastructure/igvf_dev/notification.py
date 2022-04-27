6a05d672ef25c432fa5864f1f0c5449b626f0c38
Keenan Graham <ksgraham@stanford.edu>
2022-04-26 19:55:03 -0700

    Add chatbot arn

---
+++ src/shared_infrastructure/igvf_dev/notification.py
@@ -0,0 +1,14 @@
+from constructs import Construct
+
+from aws_cdk.aws_chatbot import SlackChannelConfiguration
+
+
+class Notfication(Construct):
+
+    def __init__(self, scope, construct_id, **kwargs):
+        super().__init__(scope, construct_id, **kwargs)
+        self.encode_dcc_chatbot = SlackChannelConfiguration.from_slack_channel_configuration_arn(
+            self,
+            'EncodeDCCChatbot',
+            'arn:aws:chatbot::109189702753:chat-configuration/slack-channel/aws-chatbot'
+        )
