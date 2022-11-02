62fd15b66e556bf13a250ffc9d08de6c69bf5074
keenangraham <ksgraham@stanford.edu>
2022-11-02 12:15:28 -0700

    add-portal-credentials (#2)

--- src/shared_infrastructure/igvf_dev/secret.py
+++ src/shared_infrastructure/igvf_dev/secret.py
@@ -14,3 +14,14 @@ class DockerHubCredentials(Construct):
             'DockerSecret',
             'arn:aws:secretsmanager:us-west-2:109189702753:secret:docker-hub-credentials-EStRH5',
         )
+
+
+class PortalCredentials(Construct):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs: Any) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        self.indexing_service_key = Secret.from_secret_complete_arn(
+            self,
+            'IndexingServiceKey',
+            'arn:aws:secretsmanager:us-west-2:109189702753:secret:indexing-service-portal-key-BdNl8x',
+        )
