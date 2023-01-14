c1fcc46f00f186e7e7d5ba0196445765c7efcaf8
keenangraham <ksgraham@stanford.edu>
2023-01-13 19:17:46 -0800

    IGVF-277-add-buckets (#4)

---
+++ src/shared_infrastructure/igvf_prod/policy.py
@@ -0,0 +1,21 @@
+from constructs import Construct
+
+from aws_cdk.aws_iam import ManagedPolicy
+
+from typing import Any
+
+
+class BucketAccessPolicies(Construct):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs: Any) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        self.download_igvf_files_policy = ManagedPolicy.from_managed_policy_arn(
+            self,
+            'DownloadIgvfFilesPolicy',
+            'arn:aws:iam::035226225042:policy/download-igvf-files',
+        )
+        self.upload_igvf_files_policy = ManagedPolicy.from_managed_policy_arn(
+            self,
+            'UploadIgvfFilesPolicy',
+            'arn:aws:iam::035226225042:policy/upload-igvf-files'
+        )
